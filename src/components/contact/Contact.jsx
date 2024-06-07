74c05a8b3a0dc04beb0bcca5dafd886524172327
akshatbhansali <akshatb2198@gmail.com>
2024-06-07 12:45:07 +0530

    done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -62,7 +62,7 @@ const Contact = () => {
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>
-          <span>Mumbai</span>
+          <span>Mumbai, India</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
